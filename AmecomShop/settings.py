e8410995f257b6d1cfd8f3cd0653088572240ec4
Pagante <55301195+Pagante@users.noreply.github.com>
2021-05-22 21:56:19 +0100

    Account Authentication

--- AmecomShop/settings.py
+++ AmecomShop/settings.py
@@ -138,3 +138,19 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
+
+#SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'amecomglobalenterp@gmail.com'
+EMAIL_HOST_PASSWORD = 'happyzone' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'default from email'
--- store/views.py
+++ store/views.py
@@ -17,7 +17,7 @@ def store(request, category_slug = None):
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category = categories, is_available = True)
 
-        paginator = Paginator(products, 1)
+        paginator = Paginator(products, 3)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
 
@@ -26,7 +26,7 @@ def store(request, category_slug = None):
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
 
-        paginator = Paginator(products, 6)
+        paginator = Paginator(products, 3)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
 
